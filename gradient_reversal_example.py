c940dabf6da36e62d70377e0ec01bbb9efbafa16
elia migliore <migliorelia@gmail.com>
2019-12-22 18:51:37 +0100

    another
    fucking error

--- gradient_reversal_example.py
+++ gradient_reversal_example.py
@@ -1,7 +1,10 @@
 import torch
 import torch.nn as nn
 from torch.autograd import Function
-from torch.utils import load_state_dict_from_url
+try:
+    from torch.hub import load_state_dict_from_url
+except ImportError:
+    from torch.utils.model_zoo import load_url as load_state_dict_from_url
 
 
 __all__ = ['AlexNet', 'alexnet']
